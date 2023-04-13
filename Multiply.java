f978b62f71340781844703847d5a99e11c6ebdd5
Aiswarya <aiswaryaa19@gmail.com>
2023-04-13 20:08:13 +0530

    added message in Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+    public static void main(String[] args){
+        int a =6;
+        int b=8;
+        System.out.println("The multiply is"+a*b);
+        
+        }
+    
+}
